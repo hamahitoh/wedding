4381f2ee8d4467bdc7418b1501e9a74403b71e74
hhokyo <hhokyo@gmail.com>
2026-05-09 14:59:31 -0700

    Add wedding venue details

--- js/scripts.js
+++ js/scripts.js
@@ -203,7 +203,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var location = {lat: 33.52965, lng: -117.014909};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
